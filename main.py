746266e7feb98b0999e29d06be2790916d4b9054
SweetIshi <redtwowo@gmail.com>
2024-03-02 21:31:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello')
+import pygame
+import sys
+
+pygame.init()
+
+Main_Window = pygame.display.set_mode((600,800))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
